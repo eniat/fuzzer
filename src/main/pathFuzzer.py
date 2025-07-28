09649b85c64105e8886e7bc1ca0f9dfe85c00c57
enait <tainekta@gmail.com>
2025-07-28 11:04:01 +0100

    Basic skeleton added for pathFuzzer

---
+++ src/main/pathFuzzer.py
@@ -0,0 +1,29 @@
+"""
+    Imports to be added here
+"""
+
+class PathFuzzer:
+
+    def __init__(self):
+        """
+            initialize with the configuration of the fuzzer
+        """
+        pass
+
+    def loadWordlist(self):
+        """
+            To load payload from file
+        """
+        pass
+
+    def  fuzzPath(self):
+        """
+            Fuzz the URL path using the payload
+        """
+        pass
+
+    def fuzzParams(self):
+        """
+            Fuzz query params
+        """
+        pass
